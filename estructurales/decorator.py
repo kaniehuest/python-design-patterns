0b3dc29125407c448c91ae9916f9130e674dfa3a
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2024-08-13 13:44:59 -0400

    finished decorator pattern

--- estructurales/decorator.py
+++ estructurales/decorator.py
@@ -1,3 +1,48 @@
+class Component():
+    """
+    The base Component interface defines operations that can be altered by 
+    decorators.
+    """
+    def operation(self) -> str:
+        pass
+
+
+class ConcreteComponent(Component):
+    """
+    Concrete Components provide default implementations of the operations. There
+    might be several variations of these classes.
+    """
+
+    def operation(self) -> str:
+        return "ConcreteComponent"
+
+
+class Decorator(Component):
+    """
+    The base decorator class follows the same interface as the other components.
+    The primary purpose of this class is to define the wrapping interface for 
+    all concrete decorators. The default implementation of the wrapping code
+    might include a field for storing a wrapped component and the means to 
+    initialize it.
+    """
+
+    _component: Component = None
+
+    def __init__(self, component: Component) -> None:
+        self._component = component
+
+    @property
+    def component(self) -> Component:
+        """
+        The decorator delegates all the work to the wrapped component.
+        """
+
+        return self._component
+
+    def operation(self) -> str:
+        return self._component.operation()
+
+
 class ConcreteDecoratorA(Decorator):
     """
     Concrete Decorators call the wrapped object and alter its result in some way
@@ -10,13 +55,15 @@ class ConcreteDecoratorA(Decorator):
         """
         return f"ConcreteDecoratorA({self.component.operation()})"
 
+
 class ConcreteDecoratorB(Decorator):
     """
     Decorators  can execute their behaviour either before or after the call
     to the wrapped object.
     """
-    def operation1(self) -> str:
+    def operation(self) -> str:
         return f"ConcreteDecoratorB({self.component.operation()})"
+
 
 def client_code(component: Component) -> None:
     """
@@ -26,12 +73,9 @@ def client_code(component: Component) -> None:
 
     # ...
 
-    print(f"RESULT: {component.operation()}", end="")
+    print(f"RESULT: {component.operation()}")
     
     # ...
-
-
-
 
 
 if __name__ == "__main__":
@@ -39,7 +83,7 @@ if __name__ == "__main__":
     simple = ConcreteComponent()
     print("Client: I've got a simple component:")
     client_code(simple)
-    print("\n")
+    print("")
 
     # ...as well as decorator ones.
     #
@@ -48,4 +92,5 @@ if __name__ == "__main__":
     decorator1 = ConcreteDecoratorA(simple)
     decorator2 = ConcreteDecoratorB(decorator1)
     print("Client: Now I've got a decorated component:")
+    client_code(decorator1)
     client_code(decorator2)
